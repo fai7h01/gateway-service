48abdde83e562e081e4d3fe2705053be379d18df
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-14 22:53:59 +0400

    cfg

---
+++ src/main/java/com/cydeo/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.cydeo.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("user-service", r ->
+                        r.path("/user-service/**")
+                        .filters(f -> f.rewritePath("/user-service/(?<path>.*)", "/${path}"))
+                        .uri("lb://user-service"))
+                .route("user-service", r ->
+                        r.path("/project-service/**")
+                        .filters(f -> f.rewritePath("/project-service/(?<path>.*)", "/${path}"))
+                        .uri("lb://project-service"))
+                .route("user-service", r ->
+                        r.path("/task-service/**")
+                        .filters(f -> f.rewritePath("/task-service/(?<path>.*)", "/${path}"))
+                        .uri("lb://task-service"))
+                .build();
+    }
+
+}
