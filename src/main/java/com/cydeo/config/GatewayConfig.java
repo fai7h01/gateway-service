ec8e883d32059f175d3beb389c20a23ce85fdc46
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-28 18:25:53 +0400

    AuthHeaderFilter

--- src/main/java/com/cydeo/config/GatewayConfig.java
+++ src/main/java/com/cydeo/config/GatewayConfig.java
@@ -9,20 +9,26 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayConfig {
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, AuthHeaderFilter authHeaderFilter) {
 
         return builder.routes()
                 .route("user-service", r ->
                         r.path("/user-service/**")
-                        .filters(f -> f.rewritePath("/user-service/(?<path>.*)", "/${path}"))
+                        .filters(f -> f
+                                .filter(authHeaderFilter)
+                                .rewritePath("/user-service/(?<path>.*)", "/${path}"))
                         .uri("lb://user-service"))
                 .route("project-service", r ->
                         r.path("/project-service/**")
-                        .filters(f -> f.rewritePath("/project-service/(?<path>.*)", "/${path}"))
+                        .filters(f -> f
+                                .filter(authHeaderFilter)
+                                .rewritePath("/project-service/(?<path>.*)", "/${path}"))
                         .uri("lb://project-service"))
                 .route("task-service", r ->
                         r.path("/task-service/**")
-                        .filters(f -> f.rewritePath("/task-service/(?<path>.*)", "/${path}"))
+                        .filters(f -> f
+                                .filter(authHeaderFilter)
+                                .rewritePath("/task-service/(?<path>.*)", "/${path}"))
                         .uri("lb://task-service"))
                 .build();
     }
